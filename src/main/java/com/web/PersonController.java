0ec093903368cfcd668b9e7e7e4e85e75b8c8e7b
xieqiang <xieqiang@wdai.com>
2018-07-18 15:57:40 +0800

    Initial commit

---
+++ src/main/java/com/web/PersonController.java
@@ -0,0 +1,24 @@
+package com.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by WD42700 on 2018/7/18.
+ */
+@Controller
+@RequestMapping("/person")
+public class PersonController {
+
+
+    @RequestMapping("/memberInfo")
+    @ResponseBody
+    public  String memberInfo() {
+        return  "hello,world";
+    }
+
+
+
+
+}
