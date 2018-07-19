8b7c174c603b60fb33dddece2acfce5b77553a56
xieqiang <xieqiang@wdai.com>
2018-07-19 14:34:33 +0800

    jenkins自动化脚本

---
+++ src/main/java/com/dbutil/DbUtil.java
@@ -0,0 +1,14 @@
+package com.dbutil;
+
+import java.sql.Connection;
+
+/**
+ * Created by WD42700 on 2018/7/19.
+ */
+public class DbUtil {
+
+    public   void   insert(){
+        Connection   connection =null;
+    }
+
+}
